b4b8ff9a0697021e155527af3f478745f66d4b09
Omega <omegakenshin@gmail.com>
2020-11-03 15:59:21 -0600

    add: fonts

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
         accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: ProductsOverviewScreen(),
