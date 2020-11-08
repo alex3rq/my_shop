d0097cfb5ea1bcdc34d7ba9f8c78299cd024fddf
Alex <alex3rq@gmail.com>
2020-11-08 13:28:06 -0600

    update: productOverviewScreen remove unused imports

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_shop/providers/products.dart';
 import 'package:my_shop/widgets/products_grid.dart';
-import 'package:provider/provider.dart';
 
 enum FilteredOptions { Favorites, All }
 
